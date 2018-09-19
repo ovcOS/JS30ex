e1951bcefd1ea5bb6c57da9acbeb0e6664e89ac8
Oriol VidalCortes <ovidalcortes@gmail.com>
2018-09-19 20:36:24 +0200

    in process...

---
+++ drumKit/drumKit.js
@@ -0,0 +1,9 @@
+window.addEventListener('keydown', function(e){
+  const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if(!sound) return; // to stop the function if no sound
+  sound.currentTime = 0; // rewind to the start
+  sound.play();
+  key.classList.add('playing');
+});
