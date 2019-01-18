443185672f266a047d10490aa674fa795002bf27
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-01-18 13:12:16 +0100

    unfinished

--- webcamFun/script.js
+++ webcamFun/script.js
@@ -3,3 +3,11 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo(){
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(mediaStream => {
+      video.srcObject = mediaStream;
+      video.play();
+    })
+};
